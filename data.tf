24d25b4824a29f6ae5ec45e6af73722cfc3e230a
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2018-08-20 17:24:49 +0200

    fixed wrong data

--- data.tf
+++ data.tf
@@ -5,8 +5,3 @@ data "azurerm_image" "custom-image" {
   name                = "${var.custom-image-name}"
   resource_group_name = "${var.custom-image-resource-group}"
 }
-
-data "azurerm_storage_account" "core-storage-account" {
-  name                = "qatfstate"
-  resource_group_name = "${var.custom-image-resource-group}"
-}
