ced7862f0744a1fb89c7b1113bd23302915c1ac1
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2018-08-22 15:37:00 +0200

    returning latest custom image based on creation time

--- data.tf
+++ data.tf
@@ -2,6 +2,7 @@
 # data source used to read the custom image id
 #
 data "azurerm_image" "custom-image" {
-  name                = "${var.custom-image-name}"
   resource_group_name = "${var.custom-image-resource-group}"
+  name_regex          = "^elasticsearch6-\\d{4,4}-\\d{2,2}-\\d{2,2}T\\d{6,6}"
+  sort_descending     = true
 }
